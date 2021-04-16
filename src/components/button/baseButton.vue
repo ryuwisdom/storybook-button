76bdb23d00dc3edd898a8a2c54565fe1dcb623b2
류지혜 <ryujihye@lyujihyeui-MacBook-Pro.local>
2021-04-17 00:31:08 +0900

    wip

---
+++ src/components/button/baseButton.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: 'base-button',
+
+  props: {
+    size: {
+      type: String,
+      default: 'regular',
+      validator: function(value) {
+        return ['small', 'regular', 'large'].indexOf(value) !== -1;
+      },
+    },
+    outlined: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
